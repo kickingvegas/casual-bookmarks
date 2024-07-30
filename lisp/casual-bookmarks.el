3e3cc2e81a79e39851699366ab646fc9cd5f08da
Charles Choi <kickingvegas@gmail.com>
2024-07-29 20:44:39 -0700

    Amend use-package instructions
    - Amend instructions for use-package to support loading bookmark before casual-bookmark.

--- lisp/casual-bookmarks.el
+++ lisp/casual-bookmarks.el
@@ -29,6 +29,18 @@
 ;; (require 'casual-bookmarks) ;; optional
 ;; (keymap-set bookmark-bmenu-mode-map "C-o" #'casual-bookmarks-tmenu)
 
+;; Alternately with `use-package':
+;; (use-package bookmark
+;;   :ensure nil
+;;   :defer t)
+;; (use-package casual-bookmarks
+;;   :ensure t
+;;   :bind (:map bookmark-bmenu-mode-map
+;;               ("C-o" . casual-bookmarks-tmenu)
+;;               ("S" . casual-bookmarks-sortby-tmenu)
+;;               ("J" . bookmark-jump))
+;;   :after (bookmark))
+
 ;;; Code:
 (require 'bookmark)
 (require 'transient)
